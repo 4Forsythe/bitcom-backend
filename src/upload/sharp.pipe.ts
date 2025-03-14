c83792a1b64e56361c6e80547ab40827456c0dfe
4Forsythe <forsythe.dev@gmail.com>
2025-03-14 17:19:06 +0400

    Fix: add processing pipe method for correct multiple files upload

--- src/upload/sharp.pipe.ts
+++ src/upload/sharp.pipe.ts
@@ -8,20 +8,33 @@ const imageMimes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 
 @Injectable()
 export class SharpPipe
-	implements PipeTransform<Express.Multer.File, Promise<string>>
+	implements PipeTransform<Express.Multer.File, Promise<string | string[]>>
 {
-	async transform(file: Express.Multer.File): Promise<string> {
-		if (!file) throw new BadRequestException('Недостаточно файлов для загрузки')
+	async transform(
+		data: Express.Multer.File | Express.Multer.File[]
+	): Promise<string | string[]> {
+		if (!data || (Array.isArray(data) && data.length === 0))
+			throw new BadRequestException('Недостаточно файлов для загрузки')
 
-		if (imageMimes.includes(file.mimetype)) {
-			const fileName = file.originalname
-			const fileBuffer = await fs.readFile(file.path)
-
-			await sharp(fileBuffer)
-				.jpeg({ quality: 80, progressive: true, force: true })
-				.toFile(path.join('static', fileName))
-
-			return fileName
+		if (Array.isArray(data)) {
+			return Promise.all(data.map((file) => this.process(file)))
 		}
+
+		return this.process(data)
+	}
+
+	private async process(file: Express.Multer.File): Promise<string> {
+		if (!imageMimes.includes(file.mimetype)) {
+			throw new BadRequestException('Недопустимый формат файла')
+		}
+
+		const fileName = file.originalname
+		const fileBuffer = await fs.readFile(file.path)
+
+		await sharp(fileBuffer)
+			.jpeg({ quality: 80, progressive: true, force: true })
+			.toFile(path.join('static', fileName))
+
+		return fileName
 	}
 }
